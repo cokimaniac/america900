dbfa7312bc43619b8d1e6c104def4931479129b3
Jorge Luis <cokimaniac@gmail.com>
2020-11-25 21:16:52 -0400

    Updates

---
+++ precache-manifest.ad6ffcddacb5a49b541428cedba15fd5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe5307d91ea37aefde6132c1e9ba05f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "804db4c0cae7355dd6ee",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "b5a18808b80ac9cd31e8",
+    "url": "/static/css/main.0fb3ac6a.chunk.css"
+  },
+  {
+    "revision": "804db4c0cae7355dd6ee",
+    "url": "/static/js/2.ea3c8d5e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ea3c8d5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5a18808b80ac9cd31e8",
+    "url": "/static/js/main.8c41ebe4.chunk.js"
+  },
+  {
+    "revision": "8f3b5450b48c454d090e",
+    "url": "/static/js/runtime-main.9de298f8.js"
+  }
+]);
